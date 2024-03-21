618556ab2ccaee1e84f5a0879a722a4c09d1df8b
onur <onur@DESKTOP-L534D8S>
2024-03-21 15:27:18 +0300

    ocelot + docker

--- Gateway.GatewayAPI/Program.cs
+++ Gateway.GatewayAPI/Program.cs
@@ -1,4 +1,5 @@
 using Consul;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
@@ -8,13 +9,23 @@ using Ocelot.Values;
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseContentRoot(Directory.GetCurrentDirectory());
 
-
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, o =>
+    {
+        o.MetadataAddress = "http://localhost:5053/identity/realms/secured/.well-known/openid-configuration";
+        o.RequireHttpsMetadata = false;
+        o.Authority = "http://localhost:5053/realms/secured";
+        o.Audience = "account";
+    });
+
 builder.Services.AddOcelot(builder.Configuration).AddConsul();
 builder.Configuration.AddJsonFile("ocelot.json");
 
